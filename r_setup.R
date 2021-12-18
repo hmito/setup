79aa2d92c28ad9bd0d9cf391e0fe8bb6d68502da
Koichi Ito <hmito@outlook.com>
2021-12-18 12:19:14 +0900

    Add r setup script.

---
+++ r_setup.R
@@ -0,0 +1,9 @@
+install.packages("devtools")
+install.packages("Rcpp")
+install.packages("BH")
+install.packages("tidyverse")
+install.packages("tidyquant")
+install.packages("RColorBrewer")
+
+require("devtools")
+install_github("hmito/hmRLib")
